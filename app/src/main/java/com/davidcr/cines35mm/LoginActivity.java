26a52a5254016e7edf3945feb708eae8a001c5a8
David Valverde <dav.val.gar-77@hotmail.com>
2018-06-02 17:16:03 -0600

    * Agregada actividad de inicio

--- app/src/main/java/com/davidcr/cines35mm/LoginActivity.java
+++ app/src/main/java/com/davidcr/cines35mm/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.davidcr.cines35mm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Para saltar directo al inicio, quitar despues //TODO
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        ///////////////////////////////////////////////////////////////
     }
 }
