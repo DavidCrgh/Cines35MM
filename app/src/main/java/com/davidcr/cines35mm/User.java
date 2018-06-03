5e3fdd467d906a1466db6993ecdf53e3e5d8ddf7
Melissa Villalobos <melissavillalobos@MacBook-Air-de-Melissa.local>
2018-06-03 00:42:53 -0600

    Registro y autenticacion de usuario

---
+++ app/src/main/java/com/davidcr/cines35mm/User.java
@@ -0,0 +1,45 @@
+package com.davidcr.cines35mm;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class User {
+    private static  AtomicInteger count = new AtomicInteger(0);
+    public int id;
+    public String username ;
+    public String email;
+    public String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String username, String email, String password) {
+        this.id = count.incrementAndGet();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
